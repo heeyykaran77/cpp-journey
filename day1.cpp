a73624134d94dedfdc676ae2d39c300636873f23
Karan Tiwari <tiwarikaran986@gmail.com>
2026-06-08 10:21:54 +0530

    Day 3: C++ basics and input output

---
+++ day1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int a = 14, b=15; 
+ float pi=3.14;
+ char c='d';
+  cout<< "This is tutorial 4" << endl;
+  cout<< "Here the value of a is " <<a<< endl ;
+  cout<< "The value of b is " <<b<< endl;
+  cout<< " The value of ppi is " <<pi<< endl;
+  cout<< "The value of c is " <<c<< endl;
+
+
+
+
+
+
+    return 0;
+}
---
+++ entering.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+int main()
+
+{
+    int num1, num2;
+    cout << "Enter the num1:\n";
+    cin>>num1;
+    cout <<"enter the num2:\n";
+    cin>>num2;
+    cout <<"the sum of num1 and num2 is: "<<num1+num2<<endl;
+  return 0;
+     
+}
