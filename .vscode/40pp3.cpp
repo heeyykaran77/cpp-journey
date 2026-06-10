cc943b1b289555c7d660255db5fc9f48dfebdeec
Karan Tiwari <tiwarikaran986@gmail.com>
2026-06-10 16:22:09 +0530

    40 practice ques 2

---
+++ .vscode/40pp3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// Take radius and calculate area of circle.
+int main(){
+ double radius;
+ cout << "Enter the value of radius : ";
+ cin >> radius;
+ cout << "The area of circle is: "<<3.14*radius*radius<<endl;
+ 
+  return 0;
+}
