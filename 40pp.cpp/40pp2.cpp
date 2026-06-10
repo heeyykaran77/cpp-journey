3fd8129a28d883de2bad80e77ef15e436fd3b803
Karan Tiwari <tiwarikaran986@gmail.com>
2026-06-10 13:16:38 +0530

    40 ques practice 2

---
+++ 40pp.cpp/40pp2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ // Take length and breadth and calculate area of rectangle.
+int main(){
+ int length, breadth;
+ cout << "Enter the value of length: ";
+ cin>>length;
+ cout <<"Enter the value of breadth: ";
+ cin>>breadth;
+ cout<<"The area of rectangle is: "<<length*breadth<<endl;
+    return 0;
+}
+
+
+
+
+   
+    
