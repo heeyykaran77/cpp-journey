470c5cb6d5c590dc40d4ed7427e1c9326fd6df2b
Karan Tiwari <tiwarikaran986@gmail.com>
2026-06-12 12:48:26 +0530

    40 practice ques 10

---
+++ 40pp10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//Find largest of two numbers.
+int main(){
+   double num1 ,num2;
+   cout << "Enter the first number ";
+   cin >> num1;
+   cout<< "Enter the second number ";
+   cin >> num2;
+   if (num1 > num2)
+      cout << "The largest number is: " << num1 << endl;
+   else if (num2 > num1)
+      cout << "The largest number is: " << num2 << endl;
+   else
+      cout << "Both numbers are equal." << endl;
+   return 0;
+}
